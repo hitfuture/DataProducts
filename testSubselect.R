55f6e7c418099a0dbed043206caca272ecf43876
HITFuture <brett.r.taylor@icloud.com>
2015-11-03 06:51:05 -0700

    Making additional changes to add in the ability to show types of breaches by year.

--- testSubselect.R
+++ testSubselect.R
@@ -4,3 +4,6 @@ selectedEntityTypes <- list("Business Associate", "Health Plan") #"Healthcare Cl
 breachesRange <-breaches%>%filter(((Breach.Year >= firstYear) & (Breach.Year <= lastYear))&!is.na(Individuals.Affected))%>%filter(!is.na(Covered.Entity.Type)&!is.na(Individuals.Affected))  %>%
         group_by(Breach.Year,Covered.Entity.Type)%>% summarise(impacted=sum(Individuals.Affected))%>%ungroup()
 breachesRange <- breachesRange%>%filter(Covered.Entity.Type %in% selectedEntityTypes)
+
+
+str(breaches)
